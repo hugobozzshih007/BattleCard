5f5220b53a87785e4a063c7555599b157cfa07e4
Cynthia J <cynthiajoan@gmail.com>
2013-12-27 15:56:59 -0800

    remove renamed files

--- Assets/scripts/CommonSkill.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-interface CommonSkill{
-	void InsertSelection(Transform map);
-	IList GetSelectionRange();
-	void Execute();
-}
